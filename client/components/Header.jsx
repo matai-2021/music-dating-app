00e666f064d1dd56b5b624554c2ac1e1b60ad16e
dylan-toomey <dtoomey322@gmail.com>
2021-07-16 10:22:40 +1200

    Finish register comp

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,14 +4,15 @@ import { connect } from 'react-redux'
 
 function Header (props) {
   const { user } = props
+  console.log(user)
 
   return (
     <>
       <div>
         <nav>
-          {user
-            ? <> <Link to="/login"><h1>Sign-In</h1></Link> <Link to="/register"><h1>Register</h1></Link> </>
-            : <> <h1>Logout</h1> </>
+          {user.id
+            ? <> <h1>Logout</h1> </>
+            : <> <Link to="/login"><h1>Sign-In</h1></Link> <Link to="/register"><h1>Register</h1></Link> </>
           }
         </nav>
       </div>
