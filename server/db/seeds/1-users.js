0cb9c4fa12c208aa5b86ab0c5693431a2d7947d2
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2021-07-17 09:25:53 +1200

    add users and genres

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -39,7 +39,89 @@ exports.seed = function (knex) {
           description: 'I was ranked the first DJ on the first settlement on planet Mars',
           gender_id: 1,
           created_at: new Date(Date.now())
+        },
+        {
+          id: 5,
+          username: 'rodrigo',
+          usersecret: 'eda123',
+          fullname: 'Rodrigo',
+          description: 'Metal head to the bone',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 6,
+          username: 'koko',
+          usersecret: 'eda123',
+          fullname: 'Koko',
+          description: 'Frequent visitor to any and all opera events.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 7,
+          username: 'sujie',
+          usersecret: 'eda123',
+          fullname: 'The Suj',
+          description: 'K-pop is life',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 8,
+          username: 'eleanor',
+          usersecret: 'eda123',
+          fullname: 'Eleanor',
+          description: '10 years of playing the Piano and a big fan of Ketty Perry',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 9,
+          username: 'josh',
+          usersecret: 'eda123',
+          fullname: 'Josh',
+          description: 'People keep dreaming of me playing the guitar, so I\'v decided to fulfil their dreams',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 10,
+          username: 'rayan',
+          usersecret: 'eda123',
+          fullname: 'Rayan',
+          description: 'I\'m new here',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 11,
+          username: 'james',
+          usersecret: 'eda123',
+          fullname: 'Schwimmy',
+          description: 'A philosopher and thinker who is trying to find answers to difficult questions',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 12,
+          username: 'kris',
+          usersecret: 'eda123',
+          fullname: 'Kris',
+          description: 'My beautiful kids got me into listening into Metal genre and I don`t regret it :P',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 13,
+          username: 'kritika',
+          usersecret: 'eda123',
+          fullname: 'Kritika',
+          description: 'I view music and dancing as a mean of expressing my self',
+          gender_id: 2,
+          created_at: new Date(Date.now())
         }
+
       ])
     })
 }
--- server/db/seeds/3-users_genres.js
+++ server/db/seeds/3-users_genres.js
@@ -84,6 +84,118 @@ exports.seed = function (knex) {
           id: 17,
           user_id: 2,
           genre_id: 10
+        },
+        {
+          id: 18,
+          user_id: 6,
+          genre_id: 11
+        },
+        {
+          id: 19,
+          user_id: 6,
+          genre_id: 12
+        },
+        {
+          id: 20,
+          user_id: 7,
+          genre_id: 3
+        },
+        {
+          id: 21,
+          user_id: 7,
+          genre_id: 5
+        },
+        {
+          id: 22,
+          user_id: 8,
+          genre_id: 14
+        },
+        {
+          id: 23,
+          user_id: 8,
+          genre_id: 15
+        }, {
+          id: 24,
+          user_id: 7,
+          genre_id: 13
+        }, {
+          id: 25,
+          user_id: 8,
+          genre_id: 5
+        }, {
+          id: 26,
+          user_id: 9,
+          genre_id: 1
+        },
+        {
+          id: 27,
+          user_id: 9,
+          genre_id: 2
+        },
+        {
+          id: 28,
+          user_id: 9,
+          genre_id: 7
+        },
+        {
+          id: 29,
+          user_id: 9,
+          genre_id: 6
+        },
+        {
+          id: 30,
+          user_id: 9,
+          genre_id: 6
+        },
+        {
+          id: 31,
+          user_id: 10,
+          genre_id: 7
+        },
+        {
+          id: 32,
+          user_id: 10,
+          genre_id: 16
+        },
+        {
+          id: 33,
+          user_id: 11,
+          genre_id: 7
+        },
+        {
+          id: 34,
+          user_id: 12,
+          genre_id: 16
+        },
+        {
+          id: 35,
+          user_id: 12,
+          genre_id: 7
+        },
+        {
+          id: 36,
+          user_id: 12,
+          genre_id: 8
+        },
+        {
+          id: 37,
+          user_id: 13,
+          genre_id: 11
+        },
+        {
+          id: 38,
+          user_id: 12,
+          genre_id: 16
+        },
+        {
+          id: 39,
+          user_id: 12,
+          genre_id: 1
+        },
+        {
+          id: 40,
+          user_id: 12,
+          genre_id: 14
         }
       ])
     })
