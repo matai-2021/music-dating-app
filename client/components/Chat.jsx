849312c0845927505c9b49b9c909344fb396d585
Carter-Bardell-Munro <cartermunro11@gmail.com>
2021-07-16 16:00:44 +1200

    changing chat to work with api calls

--- client/components/Chat.jsx
+++ client/components/Chat.jsx
@@ -1,42 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { ChatEngine, ChatList, ChatCard, ChatFeed, ChatHeader, MessageBubble, IsTyping, ScrollDownBar, NewMessageForm, getOrCreateChat } from 'react-chat-engine'
+import { ChatEngine, ChatList, ChatCard, ChatFeed, ChatHeader, MessageBubble, IsTyping, ScrollDownBar, NewMessageForm } from 'react-chat-engine'
 
-const ChatPage = () => {
-  const [username, setUsername] = useState('')
-
-  function createDirectChat (creds) {
-    getOrCreateChat(
-      creds,
-      { is_direct_chat: true, usernames: [username] },
-      () => setUsername('')
-    )
-  }
-
-  function renderChatForm (creds) {
-    return (
-      <div>
-        <input
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button onClick={() => createDirectChat(creds)}>Create</button>
-      </div>
-    )
-  }
-
+const ChatPage = (props) => {
   return (
     <>
       <h2><Link to={'/'}>Matching</Link></h2>
       <ChatEngine
         height='100vh'
-        userName='dylantoomey'
+        userName='dylantoomeygaa'
         userSecret='eda123'
         projectID='7565a494-51c5-49c2-943c-7c65ca00e965'
-        renderNewChatForm={(creds) => renderChatForm(creds)}
+        renderNewChatForm={(creds) => {}}
         // Customising the UI
         renderChatList={(chatAppState) => <ChatList {...chatAppState} />}
         renderChatCard={(chat, index) => <ChatCard key={`${index}`} chat={chat} />}
