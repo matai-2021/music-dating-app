43250da5f9dccd7001f2f3f1d82f3a1bc3b20cf3
Carter-Bardell-Munro <cartermunro11@gmail.com>
2021-07-18 16:00:32 +1200

    updated seed data with new profiles

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -6,122 +6,229 @@ exports.seed = function (knex) {
       return knex('users').insert([
         {
           id: 1,
-          username: 'ahmad',
+          username: 'ahmadanwar',
           usersecret: 'eda123',
-          fullname: 'ahmad anwar',
-          description: 'I was ranked the first DJ on the first settlement on planet Mars',
+          fullname: 'Ahmad Anwar',
+          description: 'I am ranked as the first DJ on the first settlement on planet Mars.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 2,
-          username: 'westley',
+          username: 'westharrison',
           usersecret: 'eda123',
-          fullname: 'Westely',
-          description: 'I was ranked the first DJ on the first settlement on planet Mars',
+          fullname: 'Westely Harrison',
+          description: 'Love listening to lofi hip-hop radio streams on YouTube.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 3,
-          username: 'dylan',
+          username: 'dylantoomey',
           usersecret: 'eda123',
-          fullname: 'Dylan Dylan',
-          description: 'I was ranked the first DJ on the first settlement on planet Mars',
+          fullname: 'Dylan Toomey',
+          description: 'Always vibing out to Cardi B, Megan Thee Stallion, and Nicki Minaj. I like it like that.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 4,
-          username: 'carter',
+          username: 'cartermunro',
           usersecret: 'eda123',
-          fullname: 'Carter',
-          description: 'I was ranked the first DJ on the first settlement on planet Mars',
+          fullname: 'Carter Bardell-Munro',
+          description: 'Rock, hip-hop, and house is in my steady rotation.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 5,
-          username: 'rodrigo',
+          username: 'rodrigonovaes',
           usersecret: 'eda123',
-          fullname: 'Rodrigo',
-          description: 'Metal head to the bone',
+          fullname: 'Rodrigo Novaes',
+          description: 'Metal head to the bone. Black Sabbath all day!',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 6,
-          username: 'koko',
+          username: 'kokoono',
           usersecret: 'eda123',
-          fullname: 'Koko',
-          description: 'Frequent visitor to any and all opera events.',
+          fullname: 'Koko Ono',
+          description: 'Frequent visitor to any and all opera events. Susan Boyle was a standout.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 7,
-          username: 'sujie',
+          username: 'sujicho',
           usersecret: 'eda123',
           fullname: 'The Suj',
-          description: 'K-pop is life',
+          description: 'K-pop is life.',
           gender_id: 2,
           created_at: new Date(Date.now())
         },
         {
           id: 8,
-          username: 'eleanor',
+          username: 'eleanorwoodhouse',
           usersecret: 'eda123',
-          fullname: 'Eleanor',
-          description: '10 years of playing the Piano and a big fan of Ketty Perry',
+          fullname: 'Eleanor Woodhouse',
+          description: 'You\'ll find me at Charlie\'s bar signing karaoke and sipping on tequila. Carly Simon will always have a special place in my heart.',
           gender_id: 2,
           created_at: new Date(Date.now())
         },
         {
           id: 9,
-          username: 'josh',
+          username: 'joshlake',
           usersecret: 'eda123',
-          fullname: 'Josh',
-          description: 'People keep dreaming of me playing the guitar, so I\'v decided to fulfil their dreams',
+          fullname: 'Josh Lake',
+          description: 'People keep dreaming of me playing the guitar, so I\'v decided to fulfil their dreams. Watch this space, new album dropping soon.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 10,
-          username: 'rayan',
+          username: 'ryanbosh',
           usersecret: 'eda123',
-          fullname: 'Rayan',
-          description: 'I\'m new here',
+          fullname: 'Ryan Bosh',
+          description: 'I\'m new here. ',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 11,
-          username: 'james',
+          username: 'jamespearce',
           usersecret: 'eda123',
           fullname: 'Schwimmy',
-          description: 'A philosopher and thinker who is trying to find answers to difficult questions',
+          description: 'Amateur DJ in the making. Love spinning some phat house bangers and even phatter yarns.',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 12,
-          username: 'kris',
+          username: 'kriswood',
           usersecret: 'eda123',
-          fullname: 'Kris',
-          description: 'My beautiful kids got me into listening into Metal genre and I don`t regret it :P',
+          fullname: 'Kris Wood',
+          description: 'My beautiful kids got me into listening to metal and I don`t regret it. My partner says otherwise though...',
           gender_id: 1,
           created_at: new Date(Date.now())
         },
         {
           id: 13,
-          username: 'kritika',
+          username: 'kritikaanita',
           usersecret: 'eda123',
-          fullname: 'Kritika',
-          description: 'I view music and dancing as a mean of expressing my self',
+          fullname: 'Kritika Anita',
+          description: 'I view music and dancing as a mean of expressing my self. Lil Nas X brings out the best of both in me.',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 14,
+          username: 'keisuketanaka',
+          usersecret: 'eda123',
+          fullname: 'Keisuke Tanaka',
+          description: 'Cool. Ambient music would have to be a favourite. I also love to dance so anything that gets me moving.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 15,
+          username: 'meliwaigant',
+          usersecret: 'eda123',
+          fullname: 'Meli Waigant',
+          description: 'You\'ll find me drinking my mate and listening to cumbia.',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 16,
+          username: 'petervdvoorn',
+          usersecret: 'eda123',
+          fullname: 'Peter van der Voorn',
+          description: 'The only music I have time for these days is Baby Shark.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 17,
+          username: 'camferguson',
+          usersecret: 'eda123',
+          fullname: 'Cameron Ferguson',
+          description: 'I love movie soundtracks. I can\'t overstate that enough. Movie soundtracks are just so good. John Williams? Wow.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 18,
+          username: 'zacchoi',
+          usersecret: 'eda123',
+          fullname: 'Zac Choi',
+          description: 'I\'m a punk rocker at heart. Love yelling out my inner angst.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 19,
+          username: 'donsmith',
+          usersecret: 'eda123',
+          fullname: 'Don Smith',
+          description: 'Love playing around with my gadgets at home making electronic music.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 20,
+          username: 'pruesingh',
+          usersecret: 'eda123',
+          fullname: 'Prue Singh',
+          description: 'Love spinning up my record player and putting on some Bob Marley, PJ Harvey, and Cocteau Twins.',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 21,
+          username: 'lachemelvin',
+          usersecret: 'eda123',
+          fullname: 'Laché Melvin',
+          description: 'Just out here trying to expand my musical horizons. Give me all your recommendations.',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 22,
+          username: 'karelfourie',
+          usersecret: 'eda123',
+          fullname: 'Karel Fourie',
+          description: 'Synth-wave and India sitar music are my bread and butter.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 23,
+          username: 'carolynstott',
+          usersecret: 'eda123',
+          fullname: 'Carolyn Stott',
+          description: 'I\'ve been listening to a lot of Six60 lately. So talented!',
+          gender_id: 2,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 24,
+          username: 'phoenixzerin',
+          usersecret: 'eda123',
+          fullname: 'Phoenix Zerin',
+          description: 'You\'ll find me in the Top 50. I love anything that\'s catchy.',
+          gender_id: 1,
+          created_at: new Date(Date.now())
+        },
+        {
+          id: 25,
+          username: 'jaredpinfold',
+          usersecret: 'eda123',
+          fullname: 'Jared Pinfold',
+          description: 'The music I like must contain the three H\'s. Hard, heavy, and ',
           gender_id: 2,
           created_at: new Date(Date.now())
         }
-
       ])
     })
 }
--- server/db/seeds/3-users_genres.js
+++ server/db/seeds/3-users_genres.js
@@ -28,7 +28,7 @@ exports.seed = function (knex) {
         }, {
           id: 6,
           user_id: 4,
-          genre_id: 1
+          genre_id: 6
         },
         {
           id: 7,
@@ -191,6 +191,166 @@ exports.seed = function (knex) {
           id: 40,
           user_id: 12,
           genre_id: 14
+        },
+        {
+          id: 41,
+          user_id: 14,
+          genre_id: 8
+        },
+        {
+          id: 42,
+          user_id: 14,
+          genre_id: 5
+        },
+        {
+          id: 43,
+          user_id: 14,
+          genre_id: 1
+        },
+        {
+          id: 44,
+          user_id: 15,
+          genre_id: 14
+        },
+        {
+          id: 45,
+          user_id: 15,
+          genre_id: 15
+        },
+        {
+          id: 46,
+          user_id: 16,
+          genre_id: 1
+        },
+        {
+          id: 47,
+          user_id: 16,
+          genre_id: 11
+        },
+        {
+          id: 48,
+          user_id: 17,
+          genre_id: 9
+        },
+        {
+          id: 49,
+          user_id: 17,
+          genre_id: 12
+        },
+        {
+          id: 50,
+          user_id: 17,
+          genre_id: 4
+        },
+        {
+          id: 51,
+          user_id: 18,
+          genre_id: 6
+        },
+        {
+          id: 52,
+          user_id: 18,
+          genre_id: 16
+        },
+        {
+          id: 53,
+          user_id: 18,
+          genre_id: 3
+        },
+        {
+          id: 54,
+          user_id: 18,
+          genre_id: 7
+        },
+        {
+          id: 55,
+          user_id: 19,
+          genre_id: 3
+        },
+        {
+          id: 56,
+          user_id: 19,
+          genre_id: 4
+        },
+        {
+          id: 57,
+          user_id: 19,
+          genre_id: 5
+        },
+        {
+          id: 58,
+          user_id: 20,
+          genre_id: 9
+        },
+        {
+          id: 59,
+          user_id: 20,
+          genre_id: 16
+        },
+        {
+          id: 60,
+          user_id: 20,
+          genre_id: 2
+        },
+        {
+          id: 61,
+          user_id: 21,
+          genre_id: 1
+        },
+        {
+          id: 62,
+          user_id: 22,
+          genre_id: 5
+        },
+        {
+          id: 63,
+          user_id: 22,
+          genre_id: 13
+        },
+        {
+          id: 64,
+          user_id: 23,
+          genre_id: 1
+        },
+        {
+          id: 65,
+          user_id: 23,
+          genre_id: 6
+        },
+        {
+          id: 66,
+          user_id: 24,
+          genre_id: 1
+        },
+        {
+          id: 67,
+          user_id: 24,
+          genre_id: 11
+        },
+        {
+          id: 68,
+          user_id: 24,
+          genre_id: 9
+        },
+        {
+          id: 69,
+          user_id: 25,
+          genre_id: 6
+        },
+        {
+          id: 70,
+          user_id: 25,
+          genre_id: 2
+        },
+        {
+          id: 71,
+          user_id: 25,
+          genre_id: 16
+        },
+        {
+          id: 72,
+          user_id: 25,
+          genre_id: 9
         }
       ])
     })
