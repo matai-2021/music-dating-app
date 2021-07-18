f537d7a151d82ebf74d68aa05dbc4135b946a19d
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2021-07-19 11:37:18 +1200

    test get genres and update server.js

--- server/routes/users.test.js
+++ server/routes/users.test.js
@@ -2,15 +2,13 @@ const request = require('supertest')
 
 const server = require('../server')
 const db = require('../db/users')
-const { applyAuthRoutes } = require('authenticare/server')
 
 jest.mock('../db/users')
-jest.mock('authenticare/server')
 
 describe('PATCH /api/v1/users/:id', () => {
   it('responds with 200', () => {
     db.updateUser.mockImplementation(() => Promise.resolve())
-    applyAuthRoutes.mockImplementation(() => null)
+    expect.assertions(1)
     return request(server)
       .patch('/api/v1/users/1')
       .then(res => {
