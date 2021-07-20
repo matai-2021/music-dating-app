233ced20741b88ee3b7de3567b02f29f7dc799cf
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2021-07-21 10:52:05 +1200

    add end-to-end testing

---
+++ tests/e2e/e2e.test.js
@@ -0,0 +1,57 @@
+const { chromium } = require('playwright')
+const config = require('../../server/db/knexfile').development
+const db = require('knex')(config)
+const { serverUrl } = require('./index')
+
+jest.setTimeout(20000)
+
+let browser
+let page
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: false, slowMo: 500 })
+  await db.migrate.latest({ directory: './server/db/migrations' })
+})
+
+beforeEach(async () => {
+  const context = await browser.newContext()
+  page = await context.newPage()
+  await db.seed.run({ directory: './server/db/seeds' })
+})
+
+afterEach(async () => {
+  await page.close()
+})
+
+afterAll(async () => {
+  await browser.close()
+  return db.destroy()
+})
+
+test('given an existing username, the user navigates to the matching page', async () => {
+  await page.goto(serverUrl)
+  await Promise.all([
+    page.waitForNavigation(),
+    page.fill('[name=username]', 'ahmadanwar'),
+    page.click('text=Sign in')
+  ])
+  expect(await page.url()).toBe(`${serverUrl}/#/matching`)
+})
+
+test('user can navigate to profile page and upate their profile', async () => {
+  await page.goto(serverUrl)
+  await Promise.all([
+    page.waitForNavigation(),
+    page.fill('[name=username]', 'ahmadanwar'),
+    page.click('text=Sign in')
+  ])
+  await page.click('#profile', { force: true })
+  expect(await page.url()).toBe(`${serverUrl}/#/profile`)
+
+  await page.fill('[name=fullname]', 'fullname')
+  await page.fill('[name=description]', 'description')
+
+  await page.selectOption('select#genderId', { label: 'Female' })
+
+  await page.click('text=Update Information')
+  expect(await page.url()).toBe(`${serverUrl}/#/matching`)
+})
