6c80851dc331c4aec0e718f5bafb3280c44d87b3
Westley Harrison <81056217+westley-ms-harrison@users.noreply.github.com>
2021-07-21 14:25:23 +1200

    aligned table to center

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -132,13 +132,13 @@ function Register (props) {
             <option value="3">Non Binary/Other</option>
           </select>
         </label>
-        <table>
+        <table className='table-container'>
           {genres.map(genre => (
             <tr key={genre}>
               <td>
                 <input onChange={(event) => handleCheck(genre.id, event)} type="checkbox" id={genre.id} name={genre.name} value={genre.id} checked={genre.checked} />
               </td>
-              <td style={{ textAlign: 'left' }}>
+              <td className='text-align'>
                 <label key={genre.id} className="para-description" htmlFor={genre.id}>{genre.name}</label>
               </td>
             </tr>
